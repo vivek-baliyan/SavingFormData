bc23c460576541315348157525e40421965f6250
vivek-baliyan <vivekbr001@gmail.com>
2024-03-03 00:43:08 +0530

    post form data

--- app.js
+++ app.js
@@ -6,14 +6,26 @@ import { getAllItems } from "./database.js";
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.render("home", { pageName: "home" });
+});
 
 app.get("/list", async (req, res) => {
   const itemList = await getAllItems();
-  res.render("list", { itemList: itemList, moment: moment });
+  res.render("list", { itemList: itemList, moment: moment, pageName: "list" });
 });
 
-app.get("/", function (req, res) {
-  res.render("form");
+app.get("/form", function (req, res) {
+  res.render("form", { pageName: "form" });
+});
+
+app.post("/form", function (req, res) {
+  console.log(req.body);
+
+  res.render("form", { pageName: "form" });
 });
 
 app.listen(3000, function () {
